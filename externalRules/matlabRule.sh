0aff3514334c9187919eb1a51a0170aa3f0500c5
nielsSkov <v.niels.skov@gmail.com>
2018-03-23 18:48:02 +0100

    rewrote the external rule for matlab again - enabled alternative tile behaviour using splitRule.sh - all external rules are now located in the new folder externalRules

---
+++ externalRules/matlabRule.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+#listen for new and terminated nodes (windows)
+bspc subscribe node_manage | while read line; do
+
+  nodeID=$(echo $line | cut -d' ' -f4)
+  nodeName=$(xprop -id $nodeID WM_NAME)
+
+  #evenly tile matlab/simulink plots on desktop 10 (=9)
+  if [[ $nodeName == *Scope* ]] || [[ $nodeName == *Figure* ]]; then
+
+    openNodes=($(bspc query -N -d 10 -n .window))
+
+    nrOfNodes=${#openNodes[@]}
+
+    if [ $nrOfNodes -ge 2 ]; then
+      #determines largest node and its longest direction (with or hight)
+      oldMax=0
+      oldYpos=0
+      for i in $(seq 0 $(($nrOfNodes-1))); do
+
+        width=$(wattr w ${openNodes[i]})
+        higth=$(wattr h ${openNodes[i]})
+        yPos=$(wattr y ${openNodes[i]})
+
+        [ "$width" -gt "$higth" ] && max=$width direc=east || max=$higth direc=south
+
+        if [ \( $max -ge $oldMax \) -a \( $yPos -ge $oldYpos \) ]; then
+          targetNode=${openNodes[i]}
+          targetDirec=$direc
+          oldMax=$max
+          oldYpos=$yPos
+        fi
+      done
+
+      bspc node $targetNode --presel-dir $targetDirec
+      bspc node $nodeID --to-node $targetNode
+    else
+      bspc node $nodeID --to-desktop 10
+    fi
+  fi
+done
