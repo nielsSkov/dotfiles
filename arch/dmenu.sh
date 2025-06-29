30167f05304fc26a0429e5e2b32ec297a8417bfa
nielsSkov <v.niels.skov@gmail.com>
2025-06-29 23:48:04 +0200

    cleaned up arch setup

---
+++ arch/dmenu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CACHE="$HOME/.dmenu_cache_recent"
+
+MOST_USED=`sort $CACHE | uniq -c | sort -rn | colrm 1 8`
+RUN=`(echo "$MOST_USED"; dmenu_path | grep -vxF "$MOST_USED") | dmenu -fn 'Inconsolata\-g-20' "$@"` &&
+(echo $RUN; head -n 99 $CACHE) > $CACHE.$$ && mv $CACHE.$$ $CACHE
+case $RUN in
+    *) exec $RUN;;
+esac
