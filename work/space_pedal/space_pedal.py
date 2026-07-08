419bb6d09f91c6336e9fd78d08b56c5bd5023cf3
nielsSkov <v.niels.skov@gmail.com>
2026-07-08 17:16:45 +0200

    space pedal now handles disconnect and reconnect of pedal

--- work/space_pedal/space_pedal.py
+++ work/space_pedal/space_pedal.py
@@ -1,8 +1,7 @@
 #!/home/niels/.local/share/space-pedal-venv/bin/python
-from StreamDeck.DeviceManager import DeviceManager
+import time
 import uinput
-
-device = uinput.Device([uinput.KEY_SPACE, uinput.KEY_LEFTMETA, uinput.BTN_LEFT])
+from StreamDeck.DeviceManager import DeviceManager
 
 MAPPING = {
     0: uinput.KEY_SPACE,      # left pedal
@@ -10,20 +9,58 @@ MAPPING = {
     2: uinput.KEY_SPACE,      # right pedal
 }
 
+device = uinput.Device([uinput.KEY_SPACE, uinput.KEY_LEFTMETA, uinput.BTN_LEFT])
+
+
 def on_key(deck, key, state):
     if key in MAPPING:
         device.emit(MAPPING[key], 1 if state else 0)
 
-deck = DeviceManager().enumerate()[0]
-deck.open()
-deck.set_key_callback(on_key)
 
-import time
-try:
+def connect():
+    decks = DeviceManager().enumerate()
+    if not decks:
+        return None
+    deck = decks[0]
+    deck.open()
+    deck.set_key_callback(on_key)
+    return deck
+
+
+def try_connect():
+    deck = connect()
+    if not (deck and deck.is_open()):
+        time.sleep(2)
+        return None
+    return deck
+
+
+def try_disconnect(deck):
+    try:
+        deck.close()
+    except Exception:
+        pass
+    time.sleep(2)
+    return None
+
+
+def main():
+    deck = None
     while True:
-        time.sleep(1)
+        if deck is None:
+            deck = try_connect()
+            continue
+
+        if not deck.is_open():
+            deck = try_disconnect(deck)
+            continue
+
+        time.sleep(0.5)
+
+
+try:
+    main()
 except KeyboardInterrupt:
     pass
 finally:
-    deck.close()
     device.close()
